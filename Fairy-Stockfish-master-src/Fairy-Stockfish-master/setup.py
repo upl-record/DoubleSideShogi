412d5f50b538e376b979a5de01efea4f8d119b1c
upl-record <ooo020417@gmail.com>
2026-06-05 16:02:15 +0900

    Fairy-Stockfish-master-src update

---
+++ Fairy-Stockfish-master-src/Fairy-Stockfish-master/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, Extension
+from glob import glob
+import platform
+import io
+import os
+
+
+if platform.python_compiler().startswith("MSC"):
+    args = ["/std:c++17"]
+else:
+    args = ["-std=c++17", "-flto", "-Wno-date-time"]
+
+args.extend(["-DLARGEBOARDS", "-DALLVARS", "-DPRECOMPUTED_MAGICS", "-DNNUE_EMBEDDING_OFF"])
+
+if "64bit" in platform.architecture():
+    args.append("-DIS_64BIT")
+
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+]
+
+with io.open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read().strip()
+
+sources = glob("src/*.cpp") + glob("src/syzygy/*.cpp") + glob("src/nnue/*.cpp") + glob("src/nnue/features/*.cpp")
+headers = glob("src/*.h") + glob("src/syzygy/*.h") + glob("src/nnue/*.h") + glob("src/nnue/features/*.h")
+ffish_source_file = os.path.normcase("src/ffishjs.cpp")
+try:
+    sources.remove(ffish_source_file)
+except ValueError:
+    print(f"ffish_source_file {ffish_source_file} was not found in sources {sources}.")
+
+pyffish_module = Extension(
+    "pyffish",
+    sources=sources,
+    depends=headers,
+    extra_compile_args=args)
+
+setup(name="pyffish", version="0.0.89",
+      description="Fairy-Stockfish Python wrapper",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Bajusz Tamás",
+      author_email="gbtami@gmail.com",
+      license="GPL3",
+      classifiers=CLASSIFIERS,
+      url="https://github.com/gbtami/Fairy-Stockfish",
+      python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
+      ext_modules=[pyffish_module],
+      data_files=[("", ["pyffish.pyi"])]
+      )
